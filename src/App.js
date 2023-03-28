80e739d9aa8d233b89bda435100566ef235861de
Jessica Flores <jessica.isa.flores@gmail.com>
2023-03-28 18:20:38 -0300

    ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import NavBar from './component/navBar/navBar';
-
+import ItemListContaines from './component/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <ItemListContaines greeting={"Bienvidos a nuestra tienda virtual"}/>
     </div>
   );
 }
---
+++ src/component/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,11 @@
+
+
+const ItemListContaines = ({greeting}) => {
+    return(
+        <div>
+            <h1>{greeting} </h1>
+        </div>
+    )
+}
+
+export default ItemListContaines
